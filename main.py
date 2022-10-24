6732fa213fdd6f30c08565439eca5491068439d2
Pliszka <karoliniakkjk@gmail.com>
2022-10-24 14:07:38 +0200

    Switch between windows

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-
-class LoginWindow(Widget):
+class WindowManager(ScreenManager):
     pass
 
+class SignUpWindow(Screen):
+    pass
+
+# class ContentWindow(Screen):
+#     pass
+
+class LoginWindow(Screen):
+    username = ObjectProperty(None)
+    pwd = ObjectProperty(None)
+
+kv = Builder.load_file('loginSystem.kv')
 
 class LoginSystemApp(App):
 
     def build(self):
-        return LoginWindow()
+        return kv
 
 
 if __name__ == "__main__":
